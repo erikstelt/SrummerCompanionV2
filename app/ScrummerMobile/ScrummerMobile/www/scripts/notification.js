3867bb84797288bd3b7493c158f678cacaa2784c
darend <contact@omniadicta.net>
2016-11-07 10:29:33 +0100

    test

--- app/ScrummerMobile/ScrummerMobile/www/scripts/notification.js
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/notification.js
@@ -1,43 +1,58 @@
 ﻿(function() {
-    'use strict';
+	'use strict';
 
-    var Notification = {
-        /**
-         * @type {HTMLElement}
-         */
-        element: null,
-        /**
-         * @type {Number}
-         */
-        timeout: null,
-        /**
-         * Show a message to the user
-         *
-         * @param {string} message
-         */
-        show: function (message) {
-            Notification.hide();
+	var Notification = {
+		/**
+		 * @type {HTMLElement}
+		 */
+		element: null,
+		/**
+		 * @type {Number}
+		 */
+		timeout: null,
+		/**
+		 * Show a message to the user
+		 *
+		 * @param {string} message
+		 */
+		show: function (message) {
+			Notification.hide();
 
-            Notification.element.textContent = message;
+			Notification.element.textContent = message;
 
-            Notification.element.classList.add('show');
+			Notification.element.classList.add('show');
 
-            Notification.timeout = window.setTimeout(Notification.hide, 5000);
-        },
-        /**
-         * Hide the message
-         */
-        hide: function() {
-            Notification.element.classList.remove('show');
+			Notification.timeout = window.setTimeout(Notification.hide, 5000);
+		},
+		/**
+		 * Hide the message
+		 */
+		hide: function() {
+			Notification.element.classList.remove('show');
 
-            window.clearTimeout(Notification.timeout);
-        }
-    };
+			window.clearTimeout(Notification.timeout);
+		}
+	};
 
-    Notification.element = document.querySelector('.notification');
-    Notification.element.addEventListener('click', function () {
-        Notification.hide();
-    });
+	Notification.element = document.querySelector('.notification');
+	Notification.element.addEventListener('click', function () {
+		Notification.hide();
+	});
 
-    window.Notification = Notification;
+	window.Notification = Notification;
 })();
+
+function SetNotifyPanelPosition () {
+	if (document.querySelector("#widget-template").offsetHeight < 130)
+		document.querySelector("#notify-area").style.top = "88px";
+	else
+		document.querySelector("#notify-area").style.top = "137px";
+}
+
+function ShowHideNotifyPanel () {
+	SetNotifyPanelPosition();
+	if (document.querySelector("#notify-area").style.height != "350px")
+		document.querySelector("#notify-area").style.height = "350px";
+	else
+	    document.querySelector("#notify-area").style.height = "0px";
+}
